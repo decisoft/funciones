6cfa23accb05c2d7cc6318290db0f45bd25c2eb6
decisoft <myanesp@protonmail.com>
2021-03-28 11:02:39 +0100

    Add products without tabs

--- funciones.php
+++ funciones.php
@@ -392,3 +392,22 @@ function dynamic_label_change( $items, $args ) {
    } 
    return $items; 
 }
+
+/* Productos WooCommerce sin pestañas */
+function woocommerce_output_product_data_tabs() {
+$product_tabs = apply_filters( 'woocommerce_product_tabs', array() );
+if ( empty( $product_tabs ) ) return;
+echo '<div class="woocommerce-tabs wc-tabs-wrapper">';
+foreach ( $product_tabs as $key => $product_tab ) {
+?>
+<div id="tab-<?php echo esc_attr( $key ); ?>">
+<?php
+if ( isset( $product_tab['callback'] ) ) {
+call_user_func( $product_tab['callback'], $key, $product_tab );
+}
+?>
+</div>
+<?php 
+}
+echo '</div>';
+}
